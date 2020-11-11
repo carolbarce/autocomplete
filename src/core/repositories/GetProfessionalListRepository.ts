990784dea200a1c5d70432febbeda35273d05d6f
Carol Barcelo <carolina.barcelo@biko2.com>
2020-11-11 21:59:11 +0100

    Add API call to repository

--- src/core/repositories/GetProfessionalListRepository.ts
+++ src/core/repositories/GetProfessionalListRepository.ts
@@ -1,2 +1,7 @@
-export const findProfessionalListRepository = (searchTerm: string) => 
-  console.log("Call API to get professional list with param: ", searchTerm);
+import { ApiClient } from './../infraestructure/apiClient'
+
+//can be change from .env file
+const MAX_RESULT = process.env.REACT_APP_MAX_LIST_RESULT;
+
+export const findProfessionalListRepository = async (searchTerm: string) => 
+  await ApiClient.get(`/words?sp=${searchTerm}??&max=${MAX_RESULT}`);
