67954c16ea72f96da4e8e905b4d9fcd8aa3691ad
Carol Barcelo <carolina.barcelo@biko2.com>
2020-11-11 23:39:32 +0100

    Show simple list result

--- src/view/Autocomplete/Autocomplete.tsx
+++ src/view/Autocomplete/Autocomplete.tsx
@@ -4,18 +4,25 @@ import { Input } from '../../components/Input/Input'
 import { getProfessionalListService } from '../../core/services/GetProfessionalListService'
 
 export const Autocomplete = () => {
-  const getProfessionalsList = (searchTerm: string) => {
-    console.log('called from input');
-    
-    if (!searchTerm || searchTerm === '') return []
+  const [professionalList, setProfessionalList] = useState<string[]>([])
 
-    getProfessionalListService(searchTerm);
+  const getProfessionalsList = async (searchTerm: string) => {
+    if (!searchTerm || searchTerm === '') setProfessionalList([])
+    
+    const professionals = await getProfessionalListService(searchTerm);
+    setProfessionalList(professionals)
   }
 
   return (
-    <Wrapper>
-      <Label> Encuentra profesionales de confianza: </Label>
-      <Input searchProfessionals={getProfessionalsList}/>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Label> Encuentra profesionales de confianza: </Label>
+        <Input searchProfessionals={getProfessionalsList}/>
+        
+      </Wrapper>
+      <div>
+      {JSON.stringify( professionalList )}
+      </div>
+    </>
   );
 }
