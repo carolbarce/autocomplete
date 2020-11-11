6d94ef9d2cb1c8415617bd67bb4dcee14529cbd2
Carol Barcelo <carolina.barcelo@biko2.com>
2020-11-11 19:56:45 +0100

    Add global layout structure

---
+++ src/components/Layout/Layout.styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro'
+import {rem} from 'polished';
+
+export const Layout = styled.div`
+  width: 100%;
+  max-width: ${rem(1360)};
+
+  padding-right: ${rem(30)};
+  padding-left: ${rem(30)};
+
+  margin-left: auto;
+  margin-right: auto;
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding-top: ${rem(30)};
+`
