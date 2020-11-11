ae684c35f2738449b24159f88f85556e2a05de80
Carol Barcelo <carolina.barcelo@biko2.com>
2020-11-11 23:14:26 +0100

    Refactor repository file

--- src/core/repositories/ProfessionalRepository.ts
+++ src/core/repositories/ProfessionalRepository.ts
@@ -5,7 +5,11 @@ import { ProfessionalListDTO } from '../domain/ProfessionalListDTO'
 //can be change from .env file
 const MAX_RESULT = process.env.REACT_APP_MAX_LIST_RESULT;
 
-export const findProfessionalListRepository = async (searchTerm: string) => {
+const findProfessionalList = async (searchTerm: string) => {
   const professionaListResponse: ProfessionalListDTO[] = await ApiClient.get(`/words?sp=${searchTerm}??&max=${MAX_RESULT}`);
   return mapDtoToProfessionalList(professionaListResponse);  
 } 
+
+export const ProfessionalRepository = {
+  findProfessionalList
+}
