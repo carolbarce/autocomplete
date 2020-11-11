998f20b1b0964ea749e78debf3f367d208672ca8
Carol Barcelo <carolina.barcelo@biko2.com>
2020-11-11 22:42:13 +0100

    Delegate listing function to Autocomplete component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { getProfessionalListService } from '../../core/services/GetProfessionalListService'
 import { InputField, SearchIcon, InputWrapper } from './Input.styles'
 
 let TIME_OUT: number = 0;
 const DELAY_TIME: number = 700;
 
-export const Input = ()=> {
+interface Props {
+  searchProfessionals: (searchTerm: string) => void
+}
+
+export const Input = ({searchProfessionals}: Props) => {
+
+  const onSearch = () => {
+    console.log("click search icon"); 
+  }
+  
+  const onTypping = (event: React.ChangeEvent<any>) => {
+    let inputText: string = event.currentTarget.value as string;
+    inputText = inputText.trim().toLowerCase();
+    
+    clearTimeout(TIME_OUT);
+    TIME_OUT = setTimeout(() => searchProfessionals(inputText), DELAY_TIME);
+  }
+
   return (
     <InputWrapper>
       <InputField type="text" onChange={onTypping} name="autocompleteInput" id="autocompleteInput" placeholder="Qué necesitas..."/>
@@ -14,14 +30,4 @@ export const Input = ()=> {
   );
 }
 
-const onSearch = () => {
-  console.log("click search icon"); 
-}
 
-const onTypping = (event: React.ChangeEvent<any>) => {
-  const inputText: string = event.currentTarget.value as string;
-  clearTimeout(TIME_OUT);
-  TIME_OUT = setTimeout(() => searchProfessionals(inputText), DELAY_TIME);
-}
-
-const searchProfessionals = (searchTerm: string) => getProfessionalListService(searchTerm)
--- src/view/Autocomplete/Autocomplete.tsx
+++ src/view/Autocomplete/Autocomplete.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Wrapper, Label } from './Autocomplete.styles'
 import { Input } from '../../components/Input/Input'
+import { getProfessionalListService } from '../../core/services/GetProfessionalListService'
 
-export const Autocomplete = ()=> {
+export const Autocomplete = () => {
+  const getProfessionalsList = (searchTerm: string) => {
+    console.log('called from input');
+    
+    if (!searchTerm || searchTerm === '') return []
+
+    getProfessionalListService(searchTerm);
+  }
+
   return (
     <Wrapper>
       <Label> Encuentra profesionales de confianza: </Label>
-      <Input/>
+      <Input searchProfessionals={getProfessionalsList}/>
     </Wrapper>
   );
 }
